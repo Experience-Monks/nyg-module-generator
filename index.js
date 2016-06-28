276858b8188953502e4d99cdd751d9c06a4eddbc
vadim <vadim@jam3.com>
2016-06-28 14:08:19 -0400

    hardcoded templates path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports = function(prompts, globs, gen) {
+module.exports = function(prompts, globs) {
 	prompts = prompts || [];
 	globs = globs || [];
 
-	require('./lib/index')(prompts, globs, gen);
+	require('./lib/index')(prompts, globs);
 };
